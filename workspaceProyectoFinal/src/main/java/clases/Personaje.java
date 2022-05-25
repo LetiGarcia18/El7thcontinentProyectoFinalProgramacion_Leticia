f75cbce2474a09feee5bed50a856846d727b28e7
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-05-25 10:29:13 +0200

    Creado constructor de personaje.
    Creación del constructor del personaje, de manera general.

--- workspaceProyectoFinal/src/main/java/clases/Personaje.java
+++ workspaceProyectoFinal/src/main/java/clases/Personaje.java
@@ -1,7 +1,20 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Personaje {
 	
+	private String nombre;
 	private int numeroCartaPosicionado;
+	private ArrayList<cartaObjeto> inventario;
+	private short contadorEnergia;
+
+	public Personaje(String nombre, int numeroCartaPosicionado, short contadorEnergia) {
+		this.nombre = nombre;
+		this.numeroCartaPosicionado = numeroCartaPosicionado;
+		this.contadorEnergia = contadorEnergia;
+	}
+	
+
 
 }
