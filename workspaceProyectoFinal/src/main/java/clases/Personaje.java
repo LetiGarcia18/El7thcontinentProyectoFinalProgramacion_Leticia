61ad77a0c73006c47932d43e0f5438979c1c83ba
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-05-25 10:31:51 +0200

    Generados Getters y Setters de la clase Personaje

--- workspaceProyectoFinal/src/main/java/clases/Personaje.java
+++ workspaceProyectoFinal/src/main/java/clases/Personaje.java
@@ -14,6 +14,40 @@ public class Personaje {
 		this.numeroCartaPosicionado = numeroCartaPosicionado;
 		this.contadorEnergia = contadorEnergia;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumeroCartaPosicionado() {
+		return numeroCartaPosicionado;
+	}
+
+	public void setNumeroCartaPosicionado(int numeroCartaPosicionado) {
+		this.numeroCartaPosicionado = numeroCartaPosicionado;
+	}
+
+	public ArrayList<CartaObjeto> getInventario() {
+		return inventario;
+	}
+
+	public void setInventario(ArrayList<CartaObjeto> inventario) {
+		this.inventario = inventario;
+	}
+
+	public short getContadorEnergia() {
+		return contadorEnergia;
+	}
+
+	public void setContadorEnergia(short contadorEnergia) {
+		this.contadorEnergia = contadorEnergia;
+	}
+	
+	
 	
 
 
