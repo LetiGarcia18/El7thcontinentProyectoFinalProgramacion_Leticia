4e1256dcfc9f112f6f2009524eafdae2dad9c170
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-06-07 11:49:02 +0200

    Clase CartaEstado documentada por completo

--- workspaceProyectoFinal/src/main/java/clases/CartaEstado.java
+++ workspaceProyectoFinal/src/main/java/clases/CartaEstado.java
@@ -1,16 +1,33 @@
 package clases;
-
+/**
+ * Clase que hereda de la super clase Carta y va a representar los estados del personaje
+ * @author Leticia
+ *
+ */
 public class CartaEstado extends Carta{
-		
-	String textoEstado;
+	/** El título del estado que puede tener el personaje **/
+	private String tituloEstado;
 
-	public CartaEstado(int id, String numeroCarta, String textoCarta, String rutaImagen, String textoEstado) {
+	/**
+	 * Constructor de la clase CartaEstado, que se va a componer de un id, un número de carta, un texto descriptivo de la carta,
+	 * la ruta de la imagen de la carta, y un título del estado que puede tener el personaje
+	 * @param id El id de la carta
+	 * @param numeroCarta El número de la carta
+	 * @param textoCarta El texto que muestra lo que ocurre en esa carta (aparece cuando se pasa el cursor por encima de la carta)
+	 * @param rutaImagen La ruta de la imagen de la carta
+	 * @param tituloEstado El título del estado que puede tener el personaje
+	 */
+	public CartaEstado(int id, String numeroCarta, String textoCarta, String rutaImagen, String tituloEstado) {
 		super(id, numeroCarta, rutaImagen, textoCarta, false);
-		this.textoEstado = textoEstado;
+		this.tituloEstado = tituloEstado;
 		
 	}
 
-	public String getTextoEstado() {
-		return textoEstado;
+	/**
+	 * Getter del titulo del estado de la carta
+	 * @return El título del estado
+	 */
+	public String getTituloEstado() {
+		return tituloEstado;
 	}
 }
