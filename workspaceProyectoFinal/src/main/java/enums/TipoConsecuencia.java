d628a9efdd05c6669a56f6d679396a2708adba0a
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-06-08 18:21:02 +0200

    Enum TipoConsecuencia documentado por completo

--- workspaceProyectoFinal/src/main/java/enums/TipoConsecuencia.java
+++ workspaceProyectoFinal/src/main/java/enums/TipoConsecuencia.java
@@ -1,11 +1,18 @@
 package enums;
-
+/**
+ * Enum TipoConsecuencia que va a representar las consecuencias de las acciones que se van a ir realizando durante el juego.
+ * @author Leticia
+ *
+ */
 public enum TipoConsecuencia {
-	
+	/** Consecuencia que te desplaza **/
 	DESPLAZARSE,
+	/** Consecuencia que te trae una carta al tablero **/
 	TRAER_CARTA,
+	/** Consecuencia que te quita una carta del tablero **/
 	QUITAR_CARTA,
+	/** Consecuencia que restaura energía al personaje **/
 	RESTAURAR,
+	/** Consecuencia que hace ganar la partida **/
 	GANAR
-
 }
