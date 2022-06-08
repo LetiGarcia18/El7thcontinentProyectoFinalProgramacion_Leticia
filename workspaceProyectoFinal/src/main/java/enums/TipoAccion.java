2ef38e6c57613446538de9c9a2421c77ae421147
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-06-08 18:18:25 +0200

    Enum de TipoAccion documentado por completo

--- workspaceProyectoFinal/src/main/java/enums/TipoAccion.java
+++ workspaceProyectoFinal/src/main/java/enums/TipoAccion.java
@@ -1,20 +1,37 @@
 package enums;
-
+/**
+ * Enum TipoAccion que va a representar las acciones que pueden tener las cartas del juego. 
+ * @author Leticia
+ *
+ */
 public enum TipoAccion {
-	
+	/** Acción de comer **/
 	EAT,
+	/** Acción de moverse **/
 	MOVE, 
+	/** Acción de investigar **/
 	INVESTIGATE,
+	/** Acción de escalar **/
 	CLIMB,
+	/** Acción de buscar **/
 	SEARCH,
+	/** Acción de observar **/
 	OBSERVE,
+	/** Acción de cazar **/
 	HUNT,
+	/** Acción de nadar **/
 	SWIM,
+	/** Accion de tirar **/
 	PULL,
+	/** Acción de equilibrarse**/
 	BALANCE,
+	/** Acción de manipular**/
 	HANDLE,
+	/** Acción de pensar **/
 	THINK,
+	/** Acción de curarse **/
 	HEAL,
+	/** Acción de cantar **/
 	SING
 	
 }
